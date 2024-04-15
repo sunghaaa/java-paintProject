deeb8b0246431db8118992ad17f4d7c513e25694
sungha.m.dev <maengsungha@maengseonghaui-MacBookPro.local>
2024-04-15 17:03:54 +0900

    remove: View 클래스 삭제 feat: 저장 기능 추가

---
+++ src/main/java/paint/draw/SaveImages.java
@@ -0,0 +1,40 @@
+package paint.draw;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class SaveImages implements ActionListener {
+
+    private BufferedImage bufferedImage;
+
+    public SaveImages(BufferedImage bufferedImage) {
+        this.bufferedImage = bufferedImage;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setFileFilter(new FileNameExtensionFilter("*.png", "png"));
+
+        int saveOption = jFileChooser.showSaveDialog(null);
+
+        if (saveOption == JFileChooser.APPROVE_OPTION) {
+            File file = jFileChooser.getSelectedFile();
+            try {
+                ImageIO.write(bufferedImage, "png", new File(file.getAbsolutePath()));
+                System.out.println("이미지 저장 성공했습니다.");
+            } catch (IOException ioException) {
+                System.out.println("이미지 저장 실패했습니다.");
+            }
+        }
+        if (saveOption == JFileChooser.CANCEL_OPTION) {
+            System.out.println("이미지 저장 취소했습니다.");
+        }
+    }
+}
